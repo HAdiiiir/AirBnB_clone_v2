dbfe9ee37bd847606087c04b8d302cf3f4fac68f
HAdiiiir <hader.jaamal2010@gmail.com>
2024-05-02 17:24:07 -0700

    a

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,45 +1,72 @@
 #!/usr/bin/python3
-from fabric.api import local, put, run, env
-import datetime
+"""
+Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
+"""
+from os import path
+from datetime import datetime
+from fabric.api import env, local, put, run
 
-env.hosts = ['75.101.171.13', '54.174.68.191']
+env.hosts = ["34.231.110.206", "3.239.57.196"]
 
 
 def do_pack():
-    local('mkdir -p versions')
-    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_%s.tgz" % timestamp
-    local('tar -cvzf "%s" ./web_static' % filename)
-    return filename
+    """
+    A function that generates an archive
+    """
+    date = datetime.now()
+    archive = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        date.year, date.month, date.day,
+        date.hour, date.minute, date.second
+    )
+    if not path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    cmd = "cd web_static && tar -cvzf ../{} . && cd -".format(archive)
+    if local(cmd).succeeded:
+        return archive
+    return None
 
 
 def do_deploy(archive_path):
-    try:
-        file_name = archive_path.split("/")[-1]
-        file_without = file_name.split(".")[0]
-        dirname = "/data/web_static/releases/%s" % file_without
-
-        put(archive_path, "/tmp")
-        run("sudo mkdir -p %s" % dirname)
-        run("sudo tar -xzf /tmp/%s -C %s" % (file_name, dirname))
-        run("sudo rm /tmp/%s" % file_name)
-        run("sudo mv %s/web_static/* %s/" % (dirname, dirname))
-        run("sudo rm -rf %s/web_static" % dirname)
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s %s/ /data/web_static/current" % dirname)
-        return True
-    except Exception:
+    """
+    Distributes archives to web servers
+    Args:
+        archive_path: path to local archive to be uploaded
+    """
+    if not path.exists(archive_path):
         return False
+    compressedFile = archive_path.split("/")[-1]
+    fileName = compressedFile.split(".")[0]
+    upload_path = "/tmp/{}".format(compressedFile)
+    if put(archive_path, upload_path).failed:
+        return False
+    current_release = '/data/web_static/releases/{}'.format(fileName)
+    if run("rm -rf {}".format(current_release)).failed:
+        return False
+    if run("mkdir {}".format(current_release)).failed:
+        return False
+    uncompress = "tar -xzf /tmp/{} -C {}".format(
+        compressedFile, current_release
+    )
+    if run(uncompress).failed:
+        return False
+    delete_archive = "rm -f /tmp/{}".format(compressedFile)
+    if run(delete_archive).failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    relink = "ln -s {} /data/web_static/current".format(current_release)
+    if run(relink).failed:
+        return False
+    return True
 
 
 def deploy():
-    path = do_pack()
-    if (path is None):
+    """
+    Creates and distributes an archive to your web servers
+    """
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-
-    try:
-        local('ls %s' % path)
-    except:
-        return False
-
-    return do_deploy(path)
+    return do_deploy(archive_path)
