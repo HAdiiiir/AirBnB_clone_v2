9eb5f6276bb271791b20d4b666525e6ef1d81948
HAdiiiir <hader.jaamal2010@gmail.com>
2024-05-02 17:42:01 -0700

    a

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers,
+using the function do_deploy
+"""
+from fabric.api import local, put, run, env, sudo
+from datetime import datetime
+from os import path
 
-from fabric.api import put, run, env
-
-env.hosts = ['75.101.171.13', '54.174.68.191']
+env.hosts = ['54.84.162.208', '54.175.225.209']
 
 
 def do_deploy(archive_path):
+    """Function to deploy"""
+    if not path.exists(archive_path):
+        return False
     try:
-        file_name = archive_path.split("/")[-1]
-        file_without = file_name.split(".")[0]
-        dirname = "/data/web_static/releases/%s" % file_without
-
-        put(archive_path, "/tmp")
-        run("sudo mkdir -p %s" % dirname)
-        run("sudo tar -xzf /tmp/%s -C %s" % (file_name, dirname))
-        run("sudo rm /tmp/%s" % file_name)
-        run("sudo mv %s/web_static/* %s/" % (dirname, dirname))
-        run("sudo rm -rf %s/web_static" % dirname)
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s %s/ /data/web_static/current" % dirname)
+        put(archive_path, "/tmp/")
+        name = archive_path.split('/')[1].split('.')[0]
+        sudo("mkdir -p /data/web_static/releases/{}/".format(name))
+        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+             .format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        sudo("mv /data/web_static/releases/{}/web_static/*\
+             /data/web_static/releases/{}/".format(name, name))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+             .format(name))
         return True
     except Exception:
         return False
